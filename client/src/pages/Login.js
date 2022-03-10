cc7e1262a824fb2c400571d4858ca896676d05e2
Djamel Ramdani <enzonpapa@gmail.com>
2022-03-10 16:09:35 -0600

    login and signup styled

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,3 +1,17 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
@@ -37,46 +51,53 @@ const Login = (props) => {
   };
 
   return (
-    <main>
-      <div className="login-signup">
-        <div>
-          <h4>Login</h4>
-          <div>
-            <form onSubmit={handleFormSubmit}>
-              <div>
-              <input
-                className="form-input"
-                placeholder="Email Address"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              </div>
-              <div>
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              </div>
-              <div>
-              <button className="submit-button" type="submit">
-                Submit
-              </button>
-              </div>
-            </form>
-
-            {error && <div>Login failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.400'}>Forgot password?</Link>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 import Auth from '../utils/auth';
 
@@ -10,6 +27,7 @@ const Signup = () => {
     email: '',
     password: '',
   });
+  const [showPassword, setShowPassword] = useState(false);
   const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleChange = (event) => {
@@ -36,55 +54,81 @@ const Signup = () => {
   };
 
   return (
-    <main>
-      <div className="login-signup">
-        <div>
-          <h4>Sign Up</h4>
-          <div>
-            <form onSubmit={handleFormSubmit}>
-              <div>
-              <input
-                className="form-input"
-                placeholder="Username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              </div>
-              <div>
-              <input
-                className="form-input"
-                placeholder="Email Address"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              </div>
-              <div>
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              </div>
-              <button className="submit-button" type="submit">
-                Submit
-              </button>
-            </form>
+    <Flex
+    minH={'100vh'}
+    align={'center'}
+    justify={'center'}
+    bg={useColorModeValue('gray.50', 'gray.800')}>
+    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+      <Stack align={'center'}>
+        <Heading fontSize={'4xl'} textAlign={'center'}>
+          Sign up
+        </Heading>
+        <Text fontSize={'lg'} color={'gray.600'}>
+          to enjoy all of our cool features ✌️
+        </Text>
+      </Stack>
+      <Box
+        rounded={'lg'}
+        bg={useColorModeValue('white', 'gray.700')}
+        boxShadow={'lg'}
+        p={8}>
+        <Stack spacing={4}>
+          <HStack>
+            <Box>
+              <FormControl id="firstName" isRequired>
+                <FormLabel>First Name</FormLabel>
+                <Input type="text" />
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl id="lastName">
+                <FormLabel>Last Name</FormLabel>
+                <Input type="text" />
+              </FormControl>
+            </Box>
+          </HStack>
+          <FormControl id="email" isRequired>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel>Password</FormLabel>
+            <InputGroup>
+              <Input type={showPassword ? 'text' : 'password'} />
+              <InputRightElement h={'full'}>
+                <Button
+                  variant={'ghost'}
+                  onClick={() =>
+                    setShowPassword((showPassword) => !showPassword)
+                  }>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <Stack spacing={10} pt={2}>
+            <Button
+              loadingText="Submitting"
+              size="lg"
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}>
+              Sign up
+            </Button>
+          </Stack>
+          <Stack pt={6}>
+            <Text align={'center'}>
+              Already a user? <Link color={'blue.400'}>Login</Link>
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Stack>
+  </Flex>
 
-            {error && <div>Signup failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
   );
 };
 
