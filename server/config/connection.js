045d3d8e6b59591024dfc04b18a24a6843ee345e
Djamel Ramdani <enzonpapa@gmail.com>
2022-03-10 14:53:05 -0600

    database credentials updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://localhost', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://scorpiondz1999:Enzoram@cluster0.7xv8k.mongodb.net/game-project?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
