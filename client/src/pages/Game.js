49cfe991940df18652de94b6378f0981aaa349f8
Djamel Ramdani <enzonpapa@gmail.com>
2022-03-11 17:24:42 -0600

    default player and computer cards added

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -13,10 +13,24 @@ import {
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 
+import defaultCard from "../assets/default-card.png";
 
+const tabCardDefault = [
+  "dafaultCard",
+  "dafaultCard",
+  "dafaultCard",
+  "dafaultCard",
+  "dafaultCard",
+  "dafaultCard",
+  "dafaultCard",
+];
 
 const Game = () => {
-  
+  const [stateGame, setStateGame] = useState("init");
+  const [numberCards, setNumberCards] = useState(7);
+  const [deckPlayer, setDeckPlayer] = useState([]);
+  const [deckComputer, setDeckComputer] = useState([]);
+
   return (
     <main>
       <Flex align="center" justify="center">
@@ -26,16 +40,43 @@ const Game = () => {
             bg="yellow.100"
             borderRightRadius="xl"
             borderLeftRadius="xl"
+            templateRows="repeat(3, 1fr)"
+            templateColumns="repeat(1, 1fr)"
+            gap={numberCards + 1}
           >
             <GridItem
               padding="4"
               bg="gray.100"
               borderRightRadius="md"
               borderLeftRadius="md"
+              width={{
+                md: 90 * numberCards,
+                lg: 120 * numberCards,
+                xl: 150 * numberCards,
+              }}
             >
               <Center>
                 <Text fontSize="md">Player's Cards</Text>
               </Center>
+              <Box display="flex" alignItems="center">
+                {stateGame === "init"
+                  ? tabCardDefault.map((t) => {
+                      return (
+                        <Image
+                          boxSize={{ md: 110, lg: 120, xl: 150 }}
+                          src={defaultCard}
+                        ></Image>
+                      );
+                    })
+                  : deckPlayer.map((t) => {
+                      return (
+                        <Image
+                          boxSize={{ md: 110, lg: 100, xl: 150 }}
+                          src={t}
+                        ></Image>
+                      );
+                    })}
+              </Box>
             </GridItem>
             <GridItem
               padding="4"
@@ -119,10 +160,34 @@ const Game = () => {
               bg="blue.100"
               borderRightRadius="md"
               borderLeftRadius="md"
+              width={{
+                md: 90 * numberCards,
+                lg: 120 * numberCards,
+                xl: 150 * numberCards,
+              }}
             >
               <Center>
                 <Text fontSize="xl">Computer's Cards</Text>
               </Center>
+              <Box display="flex" alignItems="center">
+                {stateGame === "init"
+                  ? tabCardDefault.map((t) => {
+                      return (
+                        <Image
+                          boxSize={{ md: 110, lg: 120, xl: 150 }}
+                          src={defaultCard}
+                        ></Image>
+                      );
+                    })
+                  : deckComputer.map((t) => {
+                      return (
+                        <Image
+                          boxSize={{ md: 110, lg: 120, xl: 150 }}
+                          src={t}
+                        ></Image>
+                      );
+                    })}
+              </Box>
             </GridItem>
           </Grid>
         </Container>
